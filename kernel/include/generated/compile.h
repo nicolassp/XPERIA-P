b5b9ce99c0500b96fc02e11bbac46e46025a7a8b
NICHOLAS PANAGIOTIS CARACOULIS <nkarakoulis@gmail.com>
2013-04-06 22:23:15 +0300

    Initial Commit

---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 17 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#17 SMP PREEMPT Sat Apr 6 18:55:13 EEST 2013"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "nicolassp"
+#define LINUX_COMPILER "gcc version 4.7.3 20130226 (prerelease) (Linaro GCC 4.7-2013.03) "
